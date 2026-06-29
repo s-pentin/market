d4cea4013cf5a694f69d7fdf1c26a55884d4b197
s-pentin <s5.pentin@yandex.ru>
2026-06-29 22:36:32 +0300

    init

---
+++ src/test/java/org/market/app/MarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.market.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
