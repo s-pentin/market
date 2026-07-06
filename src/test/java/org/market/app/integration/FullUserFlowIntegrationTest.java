8560fb58be3a7b0b2dcea5af24abb5a927736f24
s-pentin <s5.pentin@yandex.ru>
2026-07-06 20:06:04 +0300

    added integration test

---
+++ src/test/java/org/market/app/integration/FullUserFlowIntegrationTest.java
@@ -0,0 +1,66 @@
+package org.market.app.integration;
+
+import org.junit.jupiter.api.Test;
+import org.market.app.infra.TestPostgresContainer;
+import org.market.app.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+@Transactional
+class FullUserFlowIntegrationTest extends TestPostgresContainer {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    /**
+     * 1. Открываем каталог
+     * 2. Добавляем товар в корзину
+     * 3. Открываем корзину
+     * 4. Оформляем заказ
+     * 5. Проверяем страницу заказа
+     * 6. Проверяем, что корзина пуста
+     */
+    @Test
+    void fullUserFlow_buyProduct_shouldCreateOrderAndClearCart() throws Exception {
+
+        Long productId = productRepository.findAll().get(0).getId();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/items")
+                        .param("id", productId.toString())
+                        .param("action", "PLUS"))
+                .andExpect(status().is3xxRedirection());
+
+        mockMvc.perform(get("/cart/items"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("items", hasSize(greaterThan(0))));
+
+        mockMvc.perform(post("/buy"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("/orders/*?newOrder=true"));
+
+        mockMvc.perform(get("/orders/1?newOrder=true"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order"));
+
+        mockMvc.perform(get("/cart/items"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("items", hasSize(0)));
+    }
+}
