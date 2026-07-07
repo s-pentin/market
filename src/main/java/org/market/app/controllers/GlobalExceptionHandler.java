2c152e263b231226cb3e0fe9a1dbde3d09536f75
s-pentin <s5.pentin@yandex.ru>
2026-07-07 14:44:45 +0300

    fix ExceptionHandler

--- src/main/java/org/market/app/controllers/GlobalExceptionHandler.java
+++ src/main/java/org/market/app/controllers/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package org.market.app.controllers;
 import org.market.app.exceptions.EmptyCartException;
 import org.market.app.exceptions.OrderNotFoundException;
 import org.market.app.exceptions.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
@@ -13,11 +15,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmptyCartException.class)
     public String handleEmptyCart(RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("error", "Empty cart");
-        return "redirect:/cart";
+        return "redirect:/cart/items";
     }
 
     @ExceptionHandler({ProductNotFoundException.class, OrderNotFoundException.class})
-    public String handleNotFound() {
-        return "not_found";
+    public ModelAndView handleNotFound() {
+        return new ModelAndView("not_found", HttpStatus.NOT_FOUND);
     }
 }
