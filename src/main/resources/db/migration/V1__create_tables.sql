7a06b7a4f4ce7c9546e6de1a81fee18b0aea9eed
s-pentin <s5.pentin@yandex.ru>
2026-07-07 17:04:38 +0300

    change schema db

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS product
 CREATE TABLE IF NOT EXISTS orders
 (
     id        BIGSERIAL PRIMARY KEY,
-    total_sum NUMERIC(10, 2)
+    total_sum NUMERIC(10, 2) NOT NULL CHECK(total_sum >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS order_items
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS order_items
     id       BIGSERIAL PRIMARY KEY,
     order_id BIGINT         NOT NULL REFERENCES orders (id),
     title    VARCHAR(200)   NOT NULL,
-    price    NUMERIC(10, 2) NOT NULL,
-    count    INTEGER        NOT NULL
+    price    NUMERIC(10, 2) NOT NULL CHECK(price > 0),
+    count    INTEGER        NOT NULL CHECK(count > 0)
 );
 
 CREATE TABLE IF NOT EXISTS cart_item
