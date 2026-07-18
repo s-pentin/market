d09ba8951ff5ce7891f84901a184a722ee89a2da
s-pentin <s5.pentin@yandex.ru>
2026-07-18 23:47:11 +0300

    updated repo

--- src/main/java/org/market/app/repositories/OrderRepository.java
+++ src/main/java/org/market/app/repositories/OrderRepository.java
@@ -1,10 +1,9 @@
 package org.market.app.repositories;
 
 import org.market.app.models.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Orders, Long> {
-
+public interface OrderRepository extends ReactiveCrudRepository<Orders, Long> {
 }
--- src/main/java/org/market/app/repositories/ProductRepository.java
+++ src/main/java/org/market/app/repositories/ProductRepository.java
@@ -1,15 +1,20 @@
 package org.market.app.repositories;
 
 import org.market.app.models.Product;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends ReactiveCrudRepository<Product, Long> {
 
-    Page<Product> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
+    Flux<Product> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
             String title, String description, Pageable pageable
     );
+
+    Mono<Long> countByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String title, String description);
+
+    Flux<Product> findAllBy(Pageable pageable);
 }
