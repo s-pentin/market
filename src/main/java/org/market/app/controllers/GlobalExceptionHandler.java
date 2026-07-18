c27019d89815f9b8a4c54cc55df1cc251573d5bf
s-pentin <s5.pentin@yandex.ru>
2026-07-18 23:57:40 +0300

    reactive controllers and exception handler

--- src/main/java/org/market/app/controllers/GlobalExceptionHandler.java
+++ src/main/java/org/market/app/controllers/GlobalExceptionHandler.java
@@ -6,21 +6,21 @@ import org.market.app.exceptions.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmptyCartException.class)
-    public String handleEmptyCart(RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("error",
-                "Корзина пуста. Добавьте хотя бы один товар перед оформлением заказа.");
-        return "redirect:/cart/items";
+    public Mono<String> handleEmptyCart() {
+        return Mono.just("redirect:/cart/items?error=empty");
     }
 
     @ExceptionHandler({ProductNotFoundException.class, OrderNotFoundException.class})
-    public ModelAndView handleNotFound() {
-        return new ModelAndView("not_found", HttpStatus.NOT_FOUND);
+    public Mono<Rendering> handleNotFound() {
+        return Mono.just(Rendering.view("not_found")
+                .status(HttpStatus.NOT_FOUND)
+                .build());
     }
 }
