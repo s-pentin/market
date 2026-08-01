3f10a27b800c537206ceab0d02c851a594ec0210
s-pentin <s5.pentin@yandex.ru>
2026-08-01 15:47:54 +0300

    added annotations

--- market-app/src/main/java/org/market/app/dto/OrderItemsDto.java
+++ market-app/src/main/java/org/market/app/dto/OrderItemsDto.java
@@ -1,12 +1,16 @@
 package org.market.app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemsDto {
     private Long id;
     private String title;
--- market-app/src/main/java/org/market/app/dto/ProductsInCart.java
+++ market-app/src/main/java/org/market/app/dto/ProductsInCart.java
@@ -1,13 +1,17 @@
 package org.market.app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductsInCart {
     List<ItemDto> items;
     BigDecimal totalCost;
