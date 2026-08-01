62e118ab962c5afe957251af2eff4f5037268788
s-pentin <s5.pentin@yandex.ru>
2026-08-01 15:47:28 +0300

    rename

--- market-app/src/test/java/org/market/app/infra/TestContainers.java
+++ market-app/src/test/java/org/market/app/infra/TestContainers.java
@@ -3,15 +3,21 @@ package org.market.app.infra;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @ImportAutoConfiguration(FlywayAutoConfiguration.class)
-public abstract class TestPostgresContainer {
+public abstract class TestContainers {
 
     @Container
     @ServiceConnection
     static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
+    @Container
+    @ServiceConnection
+    static final GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine")
+            .withExposedPorts(6379);
 }
