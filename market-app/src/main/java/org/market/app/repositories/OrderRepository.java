10223dc3b8e8ac1933bdc4b2df83b4f143f30327
s-pentin <s5.pentin@yandex.ru>
2026-08-14 18:31:55 +0300

    fix

--- market-app/src/main/java/org/market/app/repositories/OrderRepository.java
+++ market-app/src/main/java/org/market/app/repositories/OrderRepository.java
@@ -3,7 +3,11 @@ package org.market.app.repositories;
 import org.market.app.models.Orders;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface OrderRepository extends ReactiveCrudRepository<Orders, Long> {
+    Flux<Orders> findAllByUserId(Long userId);
+    Mono<Orders> findByIdAndUserId(Long id, Long userId);
 }
